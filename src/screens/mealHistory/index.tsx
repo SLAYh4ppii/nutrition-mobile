dfd72e41bcaab7923354b72e57a911afa53ed420
denizyesilirmak <denizyesilirmak@gmail.com>
2024-12-14 22:18:17 +0300

    Refactor import path for dateNavigator utility to follow consistent naming conventions

--- src/screens/mealHistory/index.tsx
+++ src/screens/mealHistory/index.tsx
@@ -3,7 +3,7 @@ import TabSelector from "@/src/components/TabSelector";
 import { format, formatDistance } from "date-fns";
 import { useState } from "react";
 import { View } from "react-native";
-import useDateRange from "./utils/dateNavigator";
+import useDateRange from "./utils/DateNavigator";
 import formatDate from "./utils/formatDate";
 import Heatmap from "@/src/components/Heatmap";
 import WeeklyChart from "@/src/components/WeeklyChart";
